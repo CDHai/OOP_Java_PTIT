2628bab0814eed13dd81e1ae63af6aceeee74e12
Cao Duy Hai <88284121+CDHai@users.noreply.github.com>
2022-09-03 11:48:02 +0700

    Add files via upload

---
+++ J01013.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.*;
+
+public class J01013 {
+
+    static int a[] = new int[2000001];
+
+    public static void Era() {
+        for (int i = 2; i * i <= 2000000; i++)
+            if (a[i] == 0)
+                for (int j = i * i; j <= 2000000; j += i)
+                    a[j] = i;
+        for (int i = 2; i <= 2000000; i++)
+            if (a[i] == 0)
+                a[i] = i;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Era();
+        Long s = 0L;
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            while (n != 1) {
+                s += a[n];
+                n /= a[n];
+            }
+        }
+        System.out.print(s);
+    }
+}
